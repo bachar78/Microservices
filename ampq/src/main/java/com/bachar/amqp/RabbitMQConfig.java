77e2512a1de05c4ef1658b75e71a52fa2e1f3dc6
bachardaowd <bachar.daowd@gmail.com>
2024-06-25 11:40:01 +0200

    configure AMQPTemplate and JacksonConverter

--- ampq/src/main/java/com/bachar/amqp/RabbitMQConfig.java
+++ ampq/src/main/java/com/bachar/amqp/RabbitMQConfig.java
@@ -1,5 +1,6 @@
 package com.bachar.amqp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,13 +10,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RabbitMQConfig {
 
+    //Component used to create connections to a RabbitMQ broker
+    private final ConnectionFactory connectionFactory;
 
-
-
+    //The ability to be able to publish message to Queue
     @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+    public AmqpTemplate amqpTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConverter());
         return rabbitTemplate;
